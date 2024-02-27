18d268f34da872006598c858c2c066f0f86374c2
Anurag Sharma <anuraguict09@gmail.com>
2024-02-27 18:12:11 +0530

    Connect to the DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connection Successfull");
+  })
+  .catch((error) => {
+    console.error("Error saving course:", error);
+  });
 
 const app = express();
 
