8c25f2b865444df4c21a44c9a49159510714d258
Anurag Sharma <anuraguict09@gmail.com>
2024-03-26 12:44:59 +0530

    adding OAuth functionality

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,15 +3,34 @@ import {AiFillGoogleCircle} from 'react-icons/ai'
 import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
 import React from 'react'
 import { app } from '../fireBase'
+import { useDispatch } from 'react-redux'
+import { signInSuccess } from '../redux/user/userSlice'
+import { useNavigate } from 'react-router-dom'
 
 export function OAuth() {
     const auth = getAuth(app);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const handleGoogleClick = async ()=>{
         const provider = new GoogleAuthProvider()
         provider.setCustomParameters({prompt : 'select_account'})
         try {
             const resultsFromGoogle = await signInWithPopup(auth, provider);
-            console.log(resultsFromGoogle);
+           console.log(resultsFromGoogle)
+            const res = await fetch('/api/auth/google',{
+                method: 'POST',
+                headers: {'Content-Type':'application/json'},
+                body: JSON.stringify({
+                    name : resultsFromGoogle.user.displayName,
+                    email : resultsFromGoogle.user.email,
+                    googlephotoURL : resultsFromGoogle.user.photoURL
+                }),
+              }) 
+              const data = res.json();
+              if(res.ok){
+                dispatch(signInSuccess(data))
+                navigate('/')
+              }
         } catch (error) {
             console.log(error)
         }
