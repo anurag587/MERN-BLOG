fa8d3f14ace7d03f48e48f753f95d14ef2074cf8
Anurag Sharma <anuraguict09@gmail.com>
2024-05-27 14:22:14 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import postRoutes from "./routes/post.routes.js";
 import commentRoutes from "./routes/comment.routes.js";
+import path from "path";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -16,6 +17,7 @@ mongoose
     console.error("Error saving course:", error);
   });
 
+  const __dirname = path.resolve()
 const app = express();
 app.use(express.json()); //app.use(express.json()) is middleware that parses incoming requests with JSON payloads
 app.use(cookieParser());
@@ -27,6 +29,12 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment",commentRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
